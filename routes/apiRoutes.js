7d3670425530ab53ad40dcdf20806ea81cd31909
J.Laro <joselarojr@gmail.com>
2021-02-15 14:18:33 -0500

    created apiRoutes/connected, attempting to troubleshoot why, (total distance covered) is returning as NaN

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout")
 const path = require("path");
-// const { sort } = require("methods");
+const { sort } = require("methods");
 
 router.get("/api/workouts", (req, res) => {
     Workout.aggregate()
 .addFields({ totalDuration: { $sum: "$exercises.duration" } })
-.then((data) => {
-    res.json(data).catch((err) => {
-        res.json(err)
-    });
+        .then((err, data) => {
+            if (err) return res.json(err)
+            return res.json(data)
     // Workout.find({}).then((data) => {
     //     return res.json(data).catch((err) => {
     //         return res.json(err)
@@ -32,31 +31,30 @@ router.put("/api/workouts/:id", (req, res) => {
             new: true
         }
     ).then((data) => {
-        return res.json(data).catch((err) => {
-            return res.json(err)
+        res.json(data)
+    }).catch((err) => {
+             res.json(err)
         });
     });
-});
-
 
 router.post("/api/workouts", (req, res) => {
     // console.log(req.body);
     Workout.create({}).then((data) => {
-         res.json(data).catch((err) => {
+        res.json(data)
+    }).catch((err) => {
              res.json(err)
         });
     });
-});
 
 router.get("/api/workouts/range", (req, res) => {
     Workout.aggregate()
         .sort({ day: -1 })
         .limit(7)
         .addFields({ totalDuration: { $sum: "$exercises.duration" } })
-        .then((data) => {
-            res.json(data).catch((err) => {
-                res.json(err)
-            });
+        .then((err, data) => {
+            if (err) return res.json(err)
+            return res.json(data)
+        });
     // Workout.find({}).sort({day: -1})
     //     .limit(7)
     //     .then((data) => {
@@ -64,7 +62,7 @@ router.get("/api/workouts/range", (req, res) => {
     //         res.json(err)
     //     });
     });
-});
+
 // aggregate.addFields({ salary_k: { $divide: [ "$salary", 1000 ] } });
 
 
